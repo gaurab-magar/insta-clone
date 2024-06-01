eecb4b5eb7e42a1bec451751f5252c25489de33d
Your Name <gdikshan10@gmail.com>
2024-06-02 00:46:59 +0545

    react-modal added to the navbar

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -6,10 +6,13 @@ import Input from './ui/Input';
 import { LuSend } from "react-icons/lu";
 import { signIn, useSession , signOut } from 'next-auth/react';
 import Image from 'next/image';
+import { useState } from 'react';
+import  Modal  from 'react-modal';
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const Navbar = () => {
   const {data: session} = useSession();
-  console.log(session)
+  const [isOpen, setIsOpen] = useState(false);
   return (
 
     <header className="flex h-16 w-full items-center justify-between px-4 md:px-6 border-b shadow-md sticky gap-5 ">
@@ -22,29 +25,27 @@ const Navbar = () => {
           <LuSend className="text-xl text-pink-500" />
         </button>
       </div>
-      <button type="button" onClick={() => signIn()}>
-        {session ? (
-          <div className="rounded-full overflow-hidden">
-            <Image onClick={()=>signOut()} src={session.user.image} alt={session.user.name} width={40} height={40} />
-          </div>
-        ) : (
-          <FaUser className="text-xl" />
-        )}
-    </button>
-      {/* <nav className="hidden items-center gap-4 md:flex">
-        <Link className="text-sm font-medium hover:underline hover:underline-offset-4" href="#">
-          Home
-        </Link>
-        <Link className="text-sm font-medium hover:underline hover:underline-offset-4" href="#">
-          About
-        </Link>
-        <Link className="text-sm font-medium hover:underline hover:underline-offset-4" href="#">
-          Services
-        </Link>
-        <Link className="text-sm font-medium hover:underline hover:underline-offset-4" href="#">
-          Contact
-        </Link>
-      </nav> */}
+      <div>
+      {session ? (
+            <div className='flex items-center justify-center gap-3'>
+              <IoIosAddCircleOutline onClick={()=> setIsOpen(true)} className='text-3xl text-pink-600 hover:scale-110 duration-150' />
+              <button type="button" onClick={() => signIn()}>
+                <div className="rounded-full overflow-hidden">
+                  <Image onClick={()=>signOut()} src={session.user.image} alt={session.user.name} width={40} height={40} />
+                </div>
+               </button>
+            </div>
+          ) : (
+            <FaUser className="text-xl" />
+          )}
+      </div>
+        {
+          isOpen && 
+          <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} ariaHideApp={false} className="max-w-lg  absolute top-52 left-52 w-[90%]  flex flex-col items-center justify-center gap-5 bg-white p-5 rounded-md shadow-md">
+            <h2>Modal is open!</h2>
+            <button onClick={() => setIsOpen(false)}>Close</button>
+          </Modal>
+        }
     </header>
   )
 }
